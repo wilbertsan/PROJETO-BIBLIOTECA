4b0ff823598582798a9429b75c716bbcb1316b50
Wilbert Santana dos Santos <58305929+wilbertsan@users.noreply.github.com>
2022-09-01 09:51:45 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def criar_tabela():
     banco = sqlite3.connect('table_users.db')
     cursor = banco.cursor()
     cursor.execute(
-        '''CREATE TABLE USERS (nomecompleto text, cpf text, datanascimento text, email text, telefone integer, endereco text, cep text, numero text, logradouro text, senha text, aceitecontrato text, aceitelgpd text)''')
+        '''CREATE TABLE USERS (nomecompleto text, cpf text, datanascimento text, email text, telefone integer, endereco text, cep integer, numero integer, logradouro text, senha text, aceitecontrato text, aceitelgpd text)''')
     banco.close()
 
 def inserir_dados(nomeCompleto, cpf, dataNascimento, email, telefone, endereco, cep, numero, logradouro, senha, aceiteContrato, aceiteLgpd):
