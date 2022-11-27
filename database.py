66026770bbc3dfb790c2db3d2d5d1468c66b492c
Wilbert Santana dos Santos <58305929+wilbertsan@users.noreply.github.com>
2022-11-27 08:27:03 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+from fileinput import close
 import sqlite3
 
 def criar_tabela_usuarios():
@@ -6,14 +7,6 @@ def criar_tabela_usuarios():
     cursor = banco.cursor()
     cursor.execute(
         '''CREATE TABLE USERS (nomecompleto text, cpf text, datanascimento text, email text, telefone integer, endereco text, cep integer, numero integer, logradouro text, senha text, aceitecontrato text, aceitelgpd text)''')
-    banco.close()
-
-def criar_tabela_livros():
-
-    banco = sqlite3.connect('table_livros.db')
-    cursor = banco.cursor()
-    cursor.execute(
-        '''CREATE TABLE LIVROS (id text, autor text, nomelivro text, edicao text, editora text, genero text, seccao text, prateleira text, status text)''')
     banco.close()
 
 def inserir_dados_usuarios(nomeCompleto, cpf, dataNascimento, email, telefone, endereco, cep, numero, logradouro, senha, aceiteContrato, aceiteLgpd):
@@ -28,6 +21,29 @@ def inserir_dados_usuarios(nomeCompleto, cpf, dataNascimento, email, telefone, e
         banco.close()
         return
 
+def verificar_usuario(cpf):
+    try:    
+        banco = sqlite3.connect('table_users.db')
+        cursor = banco.cursor()
+        cursor.execute("SELECT senha FROM USERS WHERE cpf='%s'"%cpf)
+        consulta=cursor.fetchall()
+        banco.close()
+        return consulta
+        
+    except:
+        banco.close()
+        return    
+
+
+###################### Código Referente aos Livros ##############################################
+def criar_tabela_livros():
+
+    banco = sqlite3.connect('table_livros.db')
+    cursor = banco.cursor()
+    cursor.execute(
+        '''CREATE TABLE LIVROS (id text, nomelivro text, autor text, edicao text, editora text, genero text, seccao text, prateleira text, status text)''')
+    banco.close()
+
 def inserir_dados_livro(autor, nomeLivro, edicao, editora, genero, seccao, prateleira, status):
 
     try:
@@ -36,9 +52,59 @@ def inserir_dados_livro(autor, nomeLivro, edicao, editora, genero, seccao, prate
         cursor.execute("SELECT id FROM LIVROS")
         contador_id=cursor.fetchall()
         id_livro=str(len(contador_id)+1)
-        cursor.execute("INSERT INTO LIVROS VALUES('"+id_livro+"','"+autor+"','"+nomeLivro+"','"+edicao+"','" +editora+"','"+genero+"','"+seccao+"','"+prateleira+"','"+status+"')")
+        cursor.execute("INSERT INTO LIVROS VALUES('"+id_livro+"','"+nomeLivro+"','"+autor+"','"+edicao+"','" +editora+"','"+genero+"','"+seccao+"','"+prateleira+"','"+status+"')")
         banco.commit()
         banco.close()
     except:
         banco.close()
         return
+def consultar_livros(autor, nomeLivro, edicao, editora, genero, seccao, prateleira, status):
+    try:    
+        banco = sqlite3.connect('table_livros.db')
+        cursor = banco.cursor()
+        lista1=[ 'nomeLivro','autor', 'edicao', 'editora', 'genero', 'seccao', 'prateleira', 'status']
+        lista2=[autor, nomeLivro, edicao, editora, genero, seccao, prateleira, status]
+        for item_string in lista1:
+            for item_consulta in lista2:
+                cursor.execute("SELECT * FROM LIVROS WHERE %s='%s'"%(item_string,item_consulta))
+                consulta=cursor.fetchall()
+                if len(consulta)!=0:
+                    banco.close()
+                    return consulta
+        return
+    except:
+        banco.close()
+        return    
+
+def deletar_livros(id):
+    try:
+        banco = sqlite3.connect('table_livros.db')
+        cursor = banco.cursor()
+        cursor.execute("DELETE FROM LIVROS WHERE id=%s"%(id))
+        banco.commit()
+        banco.close()
+    except:
+        banco.close()
+        return    
+
+def solicitar_livros(id):
+    try:
+        banco = sqlite3.connect('table_livros.db')
+        cursor = banco.cursor()
+        cursor.execute("UPDATE LIVROS SET status='Alugado' WHERE id=%s"%(id))
+        banco.commit()
+        banco.close()
+    except:
+        banco.close()
+        return
+
+def devolver_livros(id):
+    try:
+        banco = sqlite3.connect('table_livros.db')
+        cursor = banco.cursor()
+        cursor.execute("UPDATE LIVROS SET status='Disponível' WHERE id=%s"%(id))
+        banco.commit()
+        banco.close()
+    except:
+        banco.close()
+        return   
--- main.py
+++ main.py
@@ -1,11 +1,61 @@
+import re
 from flask import Flask, render_template, request
-import database
 
+import database
 app = Flask(__name__)
+
+########### ROTAS ###########
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/cadastrar_usuarios')
+def cadastrar_usuarios():
+    return render_template('cadastro_usuarios.html')
+
+@app.route('/adicionar_livros')
+def adicionar_livros():
+    return render_template('adicionar_livros.html')
+
+@app.route('/solicitar_livros')
+def solicitar_livros():
+    return render_template('solicitar_livros.html')
+
+@app.route('/devolver_livros')
+def devolver_livros():
+    return render_template('devolver_livros.html')
+
+@app.route('/pesquisar')
+def pesquisar():
+    return render_template('pesquisar.html')
+
+@app.route('/retorno_pesquisar')
+def retorno_pesquisa():
+    return render_template('retorno_pesquisar.html')
+
+@app.route('/deletar')
+def deletar_livros():
+    return render_template('deletar_livros.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/usuario')
+def usuario():
+    return render_template('usuario.html')
+
+@app.route('/usuario_nao_cadastrado')
+def usuario_nao_cadastrado():
+    return render_template('usuario_nao_cadastrado.html')
+
+@app.route('/administrador')
+def administrador():
+    return render_template('administrador.html')
+
+##############################
+########## FUNÇÕES ###########
 
 @app.route('/cadastrar', methods=["POST"])
 def cadastrar():
@@ -22,15 +72,8 @@ def cadastrar():
     senhaConfirma = request.form["confirmacao"]
     aceiteContrato = request.form["contrato"]
     aceiteLgpd = request.form["lgpd"]
-    
     database.inserir_dados_usuarios(nomeCompleto, cpf, str(dataNascimento), email, telefone, endereco, cep, numero, logradouro, senha, aceiteContrato, aceiteLgpd) 
-     
     return render_template("index.html")
-
-
-@app.route('/cadastrar_usuarios')
-def cadastrar_usuarios():
-    return render_template('cadastro_usuarios.html')
 
 @app.route('/adicionar_livro', methods=["POST"])
 def adicionar_livro():
@@ -42,15 +85,53 @@ def adicionar_livro():
     seccao = request.form["seccao"]
     prateleira = request.form["prateleira"]
     status = request.form["status"]
-    
-    
     database.inserir_dados_livro(autor, nomeLivro, edicao, editora, genero, seccao, prateleira, status)
-     
     return render_template("index.html")
 
-@app.route('/adicionar_livros')
-def adicionar_livros():
-    return render_template('adicionar_livros.html')
+@app.route('/pesquisa_livros', methods=["POST"])
+def pesquisa_livros():
+    autor = request.form["autor"]
+    nomeLivro = request.form["nomelivro"]
+    edicao = request.form["edicao"]
+    editora = request.form["editora"]
+    genero = request.form["genero"]
+    seccao = request.form["seccao"]
+    prateleira = request.form["prateleira"]
+    status = request.form["status"]
+    consulta=database.consultar_livros(autor, nomeLivro, edicao, editora, genero, seccao, prateleira, status)
+    return render_template("retorno_pesquisar.html", consulta=consulta, tamanho=len(consulta))
 
+@app.route('/deleta_livro', methods=["POST"])
+def deleta_livro():
+    id = request.form["id"]
+    database.deletar_livros(id)
+    return render_template("deletar_livros.html")
+
+@app.route('/solicita_livro', methods=["POST"])
+def solicita_livro():
+    id = request.form["id"]
+    database.solicitar_livros(id)
+    return render_template("solicitar_livros.html")
+
+@app.route('/verifica_usuario', methods=["POST"])
+def verifica_usuario():
+    cpf = request.form["cpf"]
+    senha = request.form["senha"]
+    print(cpf)
+    print(senha)
+    print(database.verificar_usuario(cpf)[0][0])
+    print(type(database.verificar_usuario(cpf)))
+    if senha == database.verificar_usuario(cpf)[0][0]:
+        return render_template("usuario.html")
+    else:
+        return render_template("usuario_nao_cadastrado.html")
+
+@app.route('/devolve_livro', methods=["POST"])
+def devolve_livro():
+    id = request.form["id"]
+    database.devolver_livros(id)
+    return render_template("devolver_livros.html")
+
+##############################
 if __name__ == '__main__':
     app.run()
